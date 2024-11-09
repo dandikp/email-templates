11acfdb4e40997c74fad278569712fc789bb96ce
Dandi Pratama <gondronglabs@gmail.com>
2024-11-09 15:12:18 +0700

    intial commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const browserSync = require('browser-sync')
+const { src, dest, watch, series, parallel } = require('gulp')
+const fileInclude = require('gulp-file-include')
+const newer = require('gulp-newer')
+
+const paths = {
+    src: './src/',
+    dist: './dist/',
+    srcAssets: './src/assets/',
+    distAssets: './dist/assets/',
+}
+
+const clean = (callback) => {
+    del.sync(paths.dist, callback())
+}
+
+const html = () => {
+    const srcPath = paths.src + '/'
+    const output = paths.dist
+
+    return src([srcPath + '*.html', srcPath + '*.png'])
+        .pipe(
+            fileInclude({
+                prefix: '@@',
+                basepath: '@file',
+                indent: true,
+            })
+        )
+        .pipe(dest(output))
+}
+
+const images = () => {
+    const output = paths.distAssets + 'images'
+    return src(paths.srcAssets + 'images/**/*')
+        .pipe(newer(output))
+        .pipe(dest(output))
+}
+
+const initBrowserSync = (callback) => {
+    const startPath = '/index.html'
+
+    browserSync.init({
+        startPath,
+        server: {
+            baseDir: paths.dist,
+            middleware: [
+                function (req, res, next) {
+                    req.method = 'GET'
+                    next()
+                },
+            ],
+        },
+    })
+
+    callback()
+}
+
+const reloadBrowserSync = (callback) => {
+    browserSync.reload()
+    callback()
+}
+
+const watchFiles = () => {
+    watch(paths.src + '*.html', series([html], reloadBrowserSync))
+}
+
+exports.default = series(html, images, parallel(watchFiles, initBrowserSync))
+exports.clean = series(clean)
+exports.build = series(clean, html, images)
